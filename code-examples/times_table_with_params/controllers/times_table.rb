ec98d1a91b018df441d9ad60518351fdab755241
Phil McMinn <phil@chiffchaff.local>
2026-02-18 10:40:39 +0000

    Adding further slides and examples for week 2

---
+++ code-examples/times_table_with_params/controllers/times_table.rb
@@ -0,0 +1,16 @@
+get "/times-table" do
+  # set @multiplicand if the "m" key is set in the params and its an integer
+  param = params.fetch("m", nil)
+  @multiplicand = param.to_i if str_digits?(param)
+
+  # set @limit via the same process
+  param = params.fetch("l", nil)
+  @limit = param.to_i if str_digits?(param)
+
+  # if we have values for @multiplicand and @limit
+  # at this point (that is, they are not nil) then we have
+  # valid parameters (else, we do not)
+  @params_valid = !@multiplicand.nil? && !@limit.nil?
+
+  erb :times_table
+end
